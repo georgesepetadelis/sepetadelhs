c88017a8dca0fe176d01c81e8e940897b690c7d8
georgesepetadelis <giorgossepetadelis11@gmail.com>
2022-04-27 15:26:57 +0300

    Projects page complete

--- src/Components/pages/AboutMe.js
+++ src/Components/pages/AboutMe.js
@@ -1,5 +1,7 @@
 import TopNavBar from "../TopBarComponent";
 import '../../css/AboutMeStyle.css'
+import github_icon from '../../assets/github_icon.png'
+import instagram_icon from '../../assets/instagram_icon.png'
 
 function AboutMe() {
     return(
@@ -18,33 +20,33 @@ function AboutMe() {
 
                 <div class="mask-shadow"></div>
                 <header>
-                    <a href="https://tutsplus.com">
-                    <img src="https://en.gravatar.com/userimage/42992842/b8e8ff24d6f1cb84bbe5c839903086bb.png?size=200" />
+                    <a href="https://github.com/georgesepetadelis/">
+                    <img src="https://pbs.twimg.com/profile_images/1377349779131396096/A_f2H35A_400x400.jpg" />
                     </a>
                     <h1 className="name">GEORGE SEPETADELIS</h1>
                     <h2>Software engineer</h2>
                 </header>
 
                 <div class="profile-bio">
-                    <p>"Terence is an avid reader of science fiction, especially anything to do with aliens and tweed jackets. Most weekends, he can be found cataloguing his collection of rodent skeletons."</p>
+                    <p>16 y/o and i'm from Greece and. I have experience with many programming languages and Frameworks for all mobile platforms like Flutter, React-Native and also native Android and iOS development with Java and Swift. Also i currently working on Unreal engine (using C++ and blueprints) and Unity for some big projects. </p>
                 </div>
 
                 <ul class="profile-social-links">
                     <li>
-                    <a href="https://twitter.com/tutsplus">
-                        <img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/210284/social-twitter.svg" />
+                    <a href="https://github.com/georgesepetadelis/">
+                        <img src={github_icon} />
                     </a>
                     </li>
                     
                     <li>
-                    <a href="https://envato.com">
-                        <img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/210284/social-envato.svg" />
+                    <a href="https://www.instagram.com/sepetadelhsss/">
+                        <img src={instagram_icon} />
                     </a>
                     </li>
 
                     <li>
-                    <a href="https://codepen.io/tutsplus">
-                        <img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/210284/social-codepen.svg" />
+                    <a href="https://twitter.com/gsepetadelis">
+                        <img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/210284/social-twitter.svg" />
                     </a>
                     </li>
                 </ul>
